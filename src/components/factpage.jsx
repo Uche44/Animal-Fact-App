2a320ae87f6bbbebefe21063ff8f288c16899a0f
Uche44 <perpetualuchechukwu808@gmail.com>
2025-01-20 09:42:19 +0100

    added favorite button functionality

--- src/components/factpage.jsx
+++ src/components/factpage.jsx
@@ -1,13 +1,22 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
-// import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
-// import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import { FaHeart } from "react-icons/fa";
+import { FaSave } from "react-icons/fa";
+
+// my api key for api ninja   PkZK8NV+pP+PaeVYU8E2ig==jHkb6rWkBfo8Lmy6
+
 const Factcard = () => {
   // for the favourite icon
 
+  const [color, setColor] = useState("#D97706");
+  const handleClick = () => {
+    setColor((prevColor) => (prevColor == "#D97706" ? "#DB2777" : "#D97706"));
+  };
+
   return (
-    <div className="fact-card h-5/6 w-[28rem] rounded-t-full bg-white -z-10 fixed top-56 flex flex-col items-center px-[4rem]">
-      <h3 className="dyk mt-40 text-3xl font-bold mb-2 text-amber-950">
+    <section className="fact-card h-5/6 w-[28rem] rounded-t-full text-center bg-white -z-10 fixed top-56 flex flex-col items-center px-[4rem] sm:w-[80%] sm:h-fit sm:rounded-[3rem] sm:static sm:px-[1rem] sm:py-[1rem]">
+      <h3 className="dyk mt-40 text-3xl font-bold mb-2 text-amber-950 sm:mt-[1rem]">
         Did You Know
       </h3>
       <p className="fact">
@@ -16,7 +25,25 @@ const Factcard = () => {
         reiciendis dignissimos at nobis labore, voluptatem doloribus vitae
         aliquam. Lorem ipsum dolor sit amet consectetur
       </p>
-    </div>
+
+      <div className="icons-btn flex justify-between mt-[2rem] w-[100%] sm:px-[2.7rem] sm:pb-[1rem]">
+        <div className="icons w-[4rem] h-[2rem] flex self-end justify-between">
+          <FaSave
+            className="text-[1.6rem] left-[1rem] text-amber-700 cursor-pointer"
+            title="save fact"
+          />
+          <FaHeart
+            style={{ color: color, transition: "color 0.3s ease" }}
+            onClick={handleClick}
+            className="text-[1.6rem] bottom-[1rem] left-[3.5rem]  cursor-pointer"
+            title="like"
+          />
+        </div>
+        <button className="bg-amber-700 h-[2.5rem] w-[6rem] text-white font-semibold cursor-pointer hover:brightness-120 rounded-[0.5rem] sm:h-[2.5rem] sm:cursor-pointer">
+          New Fact
+        </button>
+      </div>
+    </section>
   );
 };
 export default function Factpage() {
@@ -24,13 +51,15 @@ export default function Factpage() {
     <>
       <section className="fact-page flex flex-col items-center">
         {/* search icon */}
-        <div className="search fixed right-4 top-4 h-[4.5rem] w-[4.5rem] rounded-4 bg-white rounded-full flex items-center justify-center">
-          <FaSearch className="search-icon text-[3rem] text-amber-700" />
+        <div className="search fixed right-4 top-4 h-[3.5rem] w-[3.5rem] rounded-4 bg-white rounded-full flex items-center justify-center sm:h-[3rem] sm:w-[3rem]">
+          <FaSearch className="search-icon text-[2.1rem] text-amber-700 sm:text-[2rem]" />
         </div>
-
-        <div className="animal-image bg-amber-800 h-[20rem] w-5/6 border-amber-500 border-4 rounded-full mt-16 sm:h-60 sm:w-60 flex items-center justify-center">
+        <div className="animal-name w-fit h-16 flex items-center justify-center fixed top-10 shadow-slate-500 bg-white rounded-[1rem] p-2">
+          <h4 className="text-[2.8rem] font-bold text-amber-800 ">LIONS</h4>
+        </div>
+        <div className="animal-image bg-amber-800 h-[20rem] w-5/6 border-amber-500 border-4 rounded-full mt-16 sm:h-64 sm:w-64 flex items-center justify-center sm: sm:mb-[2rem]">
           <img
-            src="/public/lion.jpg"
+            src="/lion.jpg"
             className="w-[95%] h-[95%] rounded-full"
             alt=""
           />
